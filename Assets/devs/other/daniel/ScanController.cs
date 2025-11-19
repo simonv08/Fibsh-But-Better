f7078d95ebd49f4e6f6b8ee38ced98b6f49d04c4
BlackHawk <danielzegwaard@gmail.com>
2025-11-19 16:07:10 +0100

    Most recent 4 fishies
    5 didnt fit in the screen so its 4

--- Assets/devs/other/daniel/ScanController.cs
+++ Assets/devs/other/daniel/ScanController.cs
@@ -4,18 +4,26 @@ public class ScanController : MonoBehaviour
 {
     [SerializeField] private SubmarineScanner scanner;
     [SerializeField] private TargetLockUI ui;
+    [SerializeField] private RecentScansManager recentScansManager;
 
     private void OnEnable()
     {
         scanner.OnTargetLocked += ui.SetTarget;
         scanner.OnTargetLost += ui.ClearTarget;
-        scanner.OnFishScanned += ui.ShowScan;
+        scanner.OnFishScanned += HandleFishScanned;
     }
 
     private void OnDisable()
     {
         scanner.OnTargetLocked -= ui.SetTarget;
         scanner.OnTargetLost -= ui.ClearTarget;
-        scanner.OnFishScanned -= ui.ShowScan;
+        scanner.OnFishScanned -= HandleFishScanned;
+    }
+
+    private void HandleFishScanned(FishInfo fishInfo)
+    {
+        ui.ShowScan(fishInfo);
+
+        recentScansManager.AddScan(fishInfo);
     }
 }
